46d8beeda5879244fedc8803c40017b7e8e42256
SmitMaruti <smit.patel@marutitech.com>
2023-02-08 15:13:43 +0530

    GATSBY init

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Navbar = () => {
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/page-2">Page 2</Link>
+    </>
+  )
+}
+
+export default Navbar
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,17 +1,11 @@
 import * as React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Navbar from "../components/navbar"
 
 const SecondPage = () => (
-  <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+  <div>
+    <Navbar />
+    SECOND PAGE
+  </div>
 )
-
-export const Head = () => <Seo title="Page two" />
 
 export default SecondPage
